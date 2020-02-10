b82d0b80907219ae459ff543f46554f263457d22
Stefan Bettaglio <sbettaglio@gmail.com>
2020-02-10 15:09:17 -0500

    'created project!'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeworkDayOne
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World, my name is Stefan");
+    }
+  }
+}
